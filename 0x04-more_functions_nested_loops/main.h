ff4592901714a0a110d0772f9483f34b8d6bd83b
Nour74n <nourhanahmed34256@gmail.com>
2023-09-15 07:41:42 -0700

    f

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H#endif
-#define MAIN_H#endif
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,3 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
+#endif
